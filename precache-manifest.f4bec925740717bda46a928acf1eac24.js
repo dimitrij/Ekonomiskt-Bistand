3e0d9c13aca1b58cbddb185ec4b4d98d4ed1373d
Basel Munawwar <baselmunawwar@Basels-MacBook-Pro.local>
2019-09-30 14:04:15 +0200

    Updates

---
+++ precache-manifest.f4bec925740717bda46a928acf1eac24.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54e5d8bd8126548d00ddecb203e0c165",
+    "url": "/Ekonomiskt-Bistand/index.html"
+  },
+  {
+    "revision": "19e34ce6f2af46d1fc48",
+    "url": "/Ekonomiskt-Bistand/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "36d42b6c5a53bd576fed",
+    "url": "/Ekonomiskt-Bistand/static/js/2.1f9847a5.chunk.js"
+  },
+  {
+    "revision": "19e34ce6f2af46d1fc48",
+    "url": "/Ekonomiskt-Bistand/static/js/main.d7925928.chunk.js"
+  },
+  {
+    "revision": "e1634958682dc4075926",
+    "url": "/Ekonomiskt-Bistand/static/js/runtime-main.da6de6aa.js"
+  }
+]);
