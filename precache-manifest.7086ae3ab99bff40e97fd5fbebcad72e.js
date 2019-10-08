f66d7cfa570204be31f5cfa6a9f19cea26495b62
Basel Munawwar <baselmunawwar@Basels-MBP.lan>
2019-10-08 14:40:51 +0200

    Updates

--- precache-manifest.7086ae3ab99bff40e97fd5fbebcad72e.js
+++ precache-manifest.7086ae3ab99bff40e97fd5fbebcad72e.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "bd28fca8152cbfbc67fd005f3c43a0a1",
+    "revision": "34bd99d6addbc6218a67ad8fcb4eebaf",
     "url": "/Ekonomiskt-Bistand/index.html"
   },
   {
-    "revision": "d432e6f24b8f856823ab",
+    "revision": "8b4fe4edd586c5b4b258",
     "url": "/Ekonomiskt-Bistand/static/css/main.34de6062.chunk.css"
   },
   {
-    "revision": "1b092980de028019ea82",
-    "url": "/Ekonomiskt-Bistand/static/js/2.8ce98786.chunk.js"
+    "revision": "ff0b3868bedb37e7e2d9",
+    "url": "/Ekonomiskt-Bistand/static/js/2.70f8e6ac.chunk.js"
   },
   {
-    "revision": "d432e6f24b8f856823ab",
-    "url": "/Ekonomiskt-Bistand/static/js/main.f8bb911d.chunk.js"
+    "revision": "8b4fe4edd586c5b4b258",
+    "url": "/Ekonomiskt-Bistand/static/js/main.0bc45bca.chunk.js"
   },
   {
     "revision": "e1634958682dc4075926",
