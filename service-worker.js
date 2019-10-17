e8c424985f6d5d1fd0f44ed244c0ae2737c6c455
Basel Munawwar <baselmunawwar@Basels-MBP.lan>
2019-10-17 10:35:21 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Ekonomiskt-Bistand/precache-manifest.45b64085a51f2aa7eaf1f9e5971c64b8.js"
+  "/Ekonomiskt-Bistand/precache-manifest.d91e01763de24f7f2429961977255028.js"
 );
 
 self.addEventListener('message', (event) => {
